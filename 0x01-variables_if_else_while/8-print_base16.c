26fee3a0d24f537a0bdae159295a91c2e44c31b0
yeagherworks <ctrust11@gmail.com>
2023-09-12 00:11:02 -0700

    task 1st batch

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (1 = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar (j);
+	putchar('\n');
+	return (0);
+
+}
